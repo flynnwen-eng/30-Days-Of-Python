83e6b447209e7e05d4f08a864f7722a6d9de38a3
Flynnwen <flynnwen@gmail.com>
2026-03-11 22:04:56 -0600

    Began Completing portions of Operators from Day 3 learnins

---
+++ 03_Day_Operators/day-3-Flynn-Opertors.py
@@ -0,0 +1,8 @@
+print('Addition: ', 2 + 3) #5
+print('Subtraction: ', 4 - 2) #2
+print('Multiplication: ', 3 * 8) #24
+print('Division: ', 10 / 4) #2.5
+print('Division: ', 25 / 5) #5.0
+print('Divison without the remainder:', 7. // 2) # 3
+print('Modulus: ', 3 % 2) #1
+print('Exponentiation: ', 2 ** 3) #8 it means 2 * 2 * 2
