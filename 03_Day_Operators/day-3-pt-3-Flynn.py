2870f0315f325fa0805d34f8cf0e81fb580a8111
Flynnwen <flynnwen@gmail.com>
2026-04-14 09:31:15 -0400

    Completed day 3 challenges and have since begun on day 4 with strings

--- 03_Day_Operators/day-3-pt-3-Flynn.py
+++ 03_Day_Operators/day-3-pt-3-Flynn.py
@@ -63,3 +63,16 @@ print('Coding' in 'Coding For All')
 print('a inan:', 'a' in 'an')
 print('4 is 2 ** 2:', 4 is 2 ** 2)
 
+
+print(3 > 2 and 4 > 3)
+print(3 > 2 and 4< 3)
+print(3 < 2 and 4 < 3)
+print('True and True: ', True and True)
+print(3 > 2 or 4 > 3)
+print('True or False: ', True or False)
+print(not 3 > 2)
+print(not True)
+print(not False)
+print(not not True)
+print(not not False)
+
---
+++ 04_Day_Strings/day-4-Flynn-strings.py
@@ -0,0 +1,11 @@
+#creating strings
+letter = 'P'
+print(letter)
+print(len(letter))
+greeting = 'Hello, World!'
+print(greeting)
+print(len(greeting))
+sentence = "I hope you are enjoying the 30 days of Python challenge"
+print(sentence)
+
+
