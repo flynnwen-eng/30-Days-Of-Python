84fd72eb8074cec86eaa06a5033253ecbfad0b20
Flynn <flynnwen@gmail.com>
2026-06-17 21:13:49 -0600

    Continued strings for Day 4, hoping to complete shortly however did not have much time

--- 04_Day_Strings/day-4-Flynn-strings.py
+++ 04_Day_Strings/day-4-Flynn-strings.py
@@ -9,3 +9,23 @@ sentence = "I hope you are enjoying the 30 days of Python challenge"
 print(sentence)
 
 
+multiline_string = """I am a newly built multiline string. I can be very useful when I want to write a string that spans multiple lines."""
+print(multiline_string)
+
+first_name = 'Brynja'
+last_name = 'Dawson'
+space = ' '
+full_name = first_name + space + last_name
+print(full_name)
+print(len(full_name))
+
+print('I hope everyone is enjoying the Pythong Challenge. \nAre you ?')
+print('Days\tTopics\tExercises')
+print('Day 1\t5\t5')
+print('Day 2\t6\t20')
+print('Day 3\t5\t23')
+print('Day 4\t1\t35')
+print('This is a backslash symbol (\\)')
+print('Always start every programming language with "Hello, World!"')
+
+
