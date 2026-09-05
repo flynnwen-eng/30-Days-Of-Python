3a953d2928c458aa87f0b6c5b661d8d32c1eb0b2
Flynn <flynnwen@gmail.com>
2026-09-04 18:23:17 -0600

    Started more of day 5 lists and len items in the list

--- 05_Day_Lists/day-5-Flynn-lists.py
+++ 05_Day_Lists/day-5-Flynn-lists.py
@@ -1,3 +1,15 @@
 empty_list = []
 print(len(empty_list))
 
+fruits = ['banana', 'orange', 'mango', 'lemon']
+vegetables = ['tomato', 'potato', 'cabbage', 'onion', 'carrot']
+animal_products = ['milk', 'cheese', 'yoghurt', 'meat', 'butter']
+
+print('Fruits:', fruits)
+print('Number of Fruits:', len(fruits))
+print('Vegetables:', vegetables)
+print('Number of Vegetables:', len(vegetables))
+print('Animal Products:', animal_products)
+print('Number of Animal Products:', len(animal_products))
+
+
--- 05_Day_Lists/day-5-notes.md
+++ 05_Day_Lists/day-5-notes.md
@@ -1,2 +1,4 @@
 Starting with notes for day 5:
 
+learning lists with initial values, also using len() to find the number of items in a list.
+
